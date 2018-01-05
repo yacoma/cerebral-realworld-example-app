52e1d2aeda38d4425fc2f09e627f41e595578238
Henri Hulski <henri.hulski@gazeta.pl>
2018-01-05 15:05:18 +0100

    fix: fix AuthenticationError

--- src/app/errors.js
+++ src/app/errors.js
@@ -1,8 +1,15 @@
-import { CerebralError } from 'cerebral'
+import ES6Error from 'es6-error'
 
-export class AuthenticationError extends CerebralError {
+export class AuthenticationError extends ES6Error {
   constructor(message = '') {
     super(message)
     this.name = 'AuthenticationError'
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      stack: this.stack,
+    }
   }
 }
