3878f51526a855f863bfdb60b54dcbda9c9782f4
Henri Hulski <henri.hulski@gazeta.pl>
2018-01-26 18:07:18 +0100

    fix: set options for Devtools

--- src/controller.js
+++ src/controller.js
@@ -6,7 +6,7 @@ import app from './app'
 const devtools =
   process.env.NODE_ENV === 'production'
     ? null
-    : Devtools({ host: 'localhost:8585' })
+    : Devtools({ host: 'localhost:8585', reconnect: false })
 
 export default Controller(app, {
   devtools,
