f24cf780cd422c3520fe16b5b03259617ee5ba3c
Henri Hulski <henri.hulski@gazeta.pl>
2018-04-04 18:44:16 +0200

    feature: enable debugger on production build

--- src/controller.js
+++ src/controller.js
@@ -3,11 +3,6 @@ import Devtools from 'cerebral/devtools'
 
 import app from './app'
 
-const devtools =
-  process.env.NODE_ENV === 'production'
-    ? null
-    : Devtools({ host: 'localhost:8585', reconnect: false })
-
 export default Controller(app, {
-  devtools,
+  devtools: Devtools({ host: 'localhost:8585', reconnect: false }),
 })
